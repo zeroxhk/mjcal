dc5cb1c503eb56e200bb22ab69bee505b134c779
Jason Yu <me@ycmjason.com>
2022-08-12 06:46:53 +0100

    chore: bump deps

---
+++ cypress.config.ts
@@ -0,0 +1,15 @@
+import { defineConfig } from 'cypress'
+
+export default defineConfig({
+  projectId: 'soa145',
+  downloadsFolder: './src/__e2e__/downloads',
+  fixturesFolder: './src/__e2e__/fixtures',
+  screenshotsFolder: './src/__e2e__/screenshots',
+  video: false,
+  videosFolder: './src/__e2e__/videos',
+  e2e: {
+    setupNodeEvents(on, config) {},
+    baseUrl: 'http://localhost:3001',
+    specPattern: './src/**/__e2e__/*.spec.ts',
+  },
+})
